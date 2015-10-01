ba76ca88cab46743adad6b0ccba8a23c836f7f52
fengalbean <ifengdb@163.com>
2015-10-02 01:13:16 +0800

    first commit

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'TMPL_PARSE_STRING'  =>array(
+        '__LIBS__'   => '/Public/module/libs',//公用类库
+        '__FRONTED__'     => '/Public/module/home', // 前台静态文件引用地址
+        '__ADMIN__' => '/Public/module/admin', // 后台管理静态文件引用地址
+    )
+
+);
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,34 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    
+    <title>首页</title>
+
+    <link rel="stylesheet" href="/Public/module/libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/Public/module/libs/flat-ui/css/flat-ui.min.css">
+    
+    <link rel="stylesheet" href="/Public/module/home/css/index.css"/>
+
+</head>
+<body>
+
+    <div class="signature"></div>
+
+
+    <div class="nav-container">
+    <div class="nav-bar">
+        <ul class="nav-menu">
+            <li>首页</li>
+            <li>博客</li>
+            <li>关于我</li>
+        </ul>
+    </div>
+</div>
+
+主要内容
+右侧菜单
+底部版权栏
+</body>
+</html>
